5dc2f194d037415c47200a9b4c52dc45e4578f30
Jessi <17324719944@189.cn>
2023-12-16 11:22:14 +0800

    更新依赖版本

--- app/src/main/java/top/jessi/scankitdemo/DisPlayMulActivity.java
+++ app/src/main/java/top/jessi/scankitdemo/DisPlayMulActivity.java
@@ -29,7 +29,6 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.example.scankitdemo.R;
 import com.huawei.hms.ml.scan.HmsScan;
 
 
